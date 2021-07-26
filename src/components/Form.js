a1864d64ab2b175595b5b9f5c48c08b81f18c766
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-26 20:39:40 +0200

    added axios

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Form = (props) => {
 	const [username, setUsername] = useState("");
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		alert(username);
+		axios
+			.get(`https://api.github.com/users/${username}`)
+			.then((response) => {
+				console.log(response.data);
+				setUsername("");
+			});
 	};
 
 	return (
