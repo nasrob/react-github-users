e92c109ffc2ef690946320ced70184edae97e795
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-26 20:25:31 +0200

    added form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
-      <h1>
-        Search GitHub User
-      </h1>
-    </div>
-  );
+	return (
+		<div className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+			<h1>Search GitHub User</h1>
+			<Form />
+		</div>
+	);
 }
 
 export default App;
